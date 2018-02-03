eb99552e1f80edcc5879673b85cdcdfbfd65dead
fractalwrench <fractalwrench@gmail.com>
2018-02-03 15:18:03 +0000

    pass str, null, and bool tests

--- src/main/kotlin/com/fractalwrench/json2kotlin/KotlinJsonConverter.kt
+++ src/main/kotlin/com/fractalwrench/json2kotlin/KotlinJsonConverter.kt
@@ -1,6 +1,8 @@
 package com.fractalwrench.json2kotlin
 
+import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+import com.google.gson.JsonPrimitive
 import com.google.gson.JsonSyntaxException
 import com.squareup.kotlinpoet.*
 import java.io.OutputStream
@@ -17,27 +19,10 @@ class KotlinJsonConverter(val jsonParser: JsonParser) : JsonConverter {
 
             // create kotlin class
             val jsonObject = jsonElement.asJsonObject
-
-
-            val classBuilder = TypeSpec.classBuilder(rootClassName)
-                    .addModifiers(KModifier.DATA)
-            val primaryConstructor = FunSpec.constructorBuilder()
-
-            for (key in jsonObject.keySet()) {
-                val get = jsonObject.get(key)
-
-                if (get.isJsonPrimitive) {
-                    val primitive = get.asJsonPrimitive
-                    primaryConstructor.addParameter(key, String::class)
-                    val initializer = PropertySpec.builder(key, String::class)
-                            .initializer(key)
-                    classBuilder.addProperty(initializer.build())
-                }
-            }
+            val rootClass = buildClass(rootClassName, jsonObject)
 
             val sourceFile = FileSpec.builder("", rootClassName)
-                    .addType(classBuilder
-                            .primaryConstructor(primaryConstructor.build()).build())
+                    .addType(rootClass)
                     .build()
 
             val stringBuilder = StringBuilder()
@@ -47,6 +32,48 @@ class KotlinJsonConverter(val jsonParser: JsonParser) : JsonConverter {
         } catch (e: JsonSyntaxException) {
             throw IllegalArgumentException("Invalid JSON supplied", e)
         }
+    }
+
+    private fun buildClass(rootClassName: String, jsonObject: JsonObject): TypeSpec {
+        val classBuilder = TypeSpec.classBuilder(rootClassName)
+                .addModifiers(KModifier.DATA)
+        val constructor = FunSpec.constructorBuilder()
+
+        for (key in jsonObject.keySet()) {
+            val nvp = jsonObject.get(key)
+
+            when {
+                nvp.isJsonNull -> {
+                    val type = Any::class.asTypeName().asNullable()
+                    addDataClassProperty(key, type, constructor, classBuilder)
+                }
+                nvp.isJsonPrimitive -> {
+                    val primitive = nvp.asJsonPrimitive
+                    val type = typenameForPrimitive(primitive)
+                    addDataClassProperty(key, type, constructor, classBuilder)
+                }
+                nvp.isJsonArray -> TODO("Handle array")
+                nvp.isJsonObject -> TODO("Handle object")
+            }
+        }
+        return classBuilder.primaryConstructor(constructor.build()).build()
+    }
+
+    private fun typenameForPrimitive(primitive: JsonPrimitive): ClassName {
+        return when {
+            primitive.isBoolean -> Boolean::class
+            primitive.isNumber -> Number::class
+            primitive.isString -> String::class
+            else -> throw IllegalStateException("No type found for JSON primitive " + primitive)
+        }.asTypeName()
+    }
+
+    private fun addDataClassProperty(key: String, type: TypeName,
+                                     constructor: FunSpec.Builder, classBuilder: TypeSpec.Builder) {
+        constructor.addParameter(key, type)
+        val initializer = PropertySpec.builder(key, type)
+                .initializer(key)
+        classBuilder.addProperty(initializer.build())
     }
 
 }
