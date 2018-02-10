11404ef93eff2d4c189bb7b7007511b30dbb4bae
fractalwrench <fractalwrench@gmail.com>
2018-02-10 09:10:36 +0000

    add missing name gen test cases

---
+++ src/test/resources/valid/namegen/ArrayExample.kt
@@ -0,0 +1,5 @@
+data class ArrayExampleContainer(val arrayExampleField: Array<Any>)
+
+data class FirstObject(val first: String)
+data class SecondObject(val second: String)
+data class ThirdObject(val third: String)
---
+++ src/test/resources/valid/namegen/SanitiseExample.kt
@@ -0,0 +1,13 @@
+data class SanitiseExample(
+    val Testword1: String,
+    val Testword2: String,
+    val Testword3: String,
+    val Testword4: String,
+    val Testword5: String,
+    val Testword6: String,
+    val Testword7: String,
+    val Testword8: String,
+    val Testword9: String,
+    val Testword10: String
+)
+
