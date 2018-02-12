277c90d51ff465de4e17a6bce8e5356e54db3d9f
fractalwrench <fractalwrench@gmail.com>
2018-02-12 20:34:44 +0000

    add sorting test for class gen

---
+++ src/test/resources/valid/sorting/ReverseSortExample.kt
@@ -0,0 +1,16 @@
+import kotlin.String
+
+data class ReverseSortExample(
+    val anteater: Anteater,
+    val lion: Lion,
+    val man: Man,
+    val zebra: Zebra
+)
+
+data class Anteater(val test4: String)
+
+data class Lion(val test2: String)
+
+data class Man(val test3: String)
+
+data class Zebra(val test: String)
