7e8f271148064b390d805ce857789b7a249cb4af
fractalwrench <fractalwrench@gmail.com>
2018-02-16 13:53:20 +0000

    setup basic web app

--- spring/src/main/kotlin/com/fractalwrench/json2kotlin/ConversionController.kt
+++ spring/src/main/kotlin/com/fractalwrench/json2kotlin/ConversionController.kt
@@ -1,12 +1,29 @@
 package com.fractalwrench.json2kotlin
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import java.io.ByteArrayOutputStream
 
-@RestController
+
+@Controller
 class ConversionController {
 
-    @GetMapping("/conversion")
-    fun greeting() = "Hello"
+    @GetMapping("/")
+    fun displayConversionForm(model: Model): String {
+        model.addAttribute("conversionForm", ConversionForm())
+        return "conversion"
+    }
+
+    @PostMapping("/results")
+    fun convertToKotlin(model: Model, @ModelAttribute conversionForm: ConversionForm): String {
+
+        val os = ByteArrayOutputStream()
+        KotlinJsonConverter().convert(conversionForm.json, os, ConversionArgs())// TODO handle failure etc with redirect
+        model.addAttribute("kotlin", String(os.toByteArray()))
+        return "results"
+    }
 
 }
---
+++ spring/src/main/kotlin/com/fractalwrench/json2kotlin/ConversionForm.kt
@@ -0,0 +1,3 @@
+package com.fractalwrench.json2kotlin
+
+class ConversionForm(var json: String = "")
