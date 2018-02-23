1cd40abd4599120dff775af3667fbf80aa313961
fractalwrench <fractalwrench@gmail.com>
2018-02-23 09:36:23 +0000

    reduce grouping sensitivity on empty objects

--- core/src/main/kotlin/com/fractalwrench/json2kotlin/JsonFieldGrouper.kt
+++ core/src/main/kotlin/com/fractalwrench/json2kotlin/JsonFieldGrouper.kt
@@ -38,17 +38,19 @@ internal class JsonFieldGrouper {
     /**
      * Determines whether two JSON Objects on the same level of a JSON tree share the same class type.
      *
-     * The grouping strategy used here is very simple. If either of the JSON objects contain 1/3 of the same
+     * The grouping strategy used here is fairly simple. If either of the JSON objects contain 1/3 of the same
      * keys as one of the others, then each object is of the same type.
-     * The only exception to this rule is the case of an empty object.
+     *
+     * The only exception to this rule is the case of an empty object, which is considered to have 1 common key
+     * with the other class, as this allows grouping with smaller objects, but discourages it with larger ones.
      */
     private fun hasSameClassType(lhs: TypedJsonElement, rhs: TypedJsonElement): Boolean {
         val lhsKeys = lhs.asJsonObject.keySet()
         val rhsKeys = rhs.asJsonObject.keySet()
-        val emptyClasses = (lhsKeys.isEmpty() || rhsKeys.isEmpty())
+        val emptyClasses = (lhsKeys.isEmpty() || rhsKeys.isEmpty()) // FIXME can add transitive deps when not needed
 
         val keySize = if (lhsKeys.size > rhsKeys.size) lhsKeys.size else rhsKeys.size
-        val commonKeyCount = lhsKeys.intersect(rhsKeys).size
-        return emptyClasses || (commonKeyCount * 3) >= keySize // at least a third of keys must match
+        val commonKeyCount = if (emptyClasses) 1 else lhsKeys.intersect(rhsKeys).size
+        return (commonKeyCount * 3) >= keySize // at least a third of keys must match
     }
 }
