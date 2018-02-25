9ca15236fda55d397aaaedd1e1d9e0cf6cf39c86
fractalwrench <fractalwrench@gmail.com>
2018-02-25 09:42:09 +0000

    improve perf by writing to buffered stream

--- core/src/main/kotlin/com/fractalwrench/json2kotlin/SourceFileWriter.kt
+++ core/src/main/kotlin/com/fractalwrench/json2kotlin/SourceFileWriter.kt
@@ -2,13 +2,15 @@ package com.fractalwrench.json2kotlin
 
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.TypeSpec
+import java.io.BufferedWriter
 import java.io.OutputStream
+import java.io.OutputStreamWriter
 import java.util.*
 
 /**
  * Writes a collection of types to a source file OutputStream.
  */
-internal class SourceFileWriter { // TODO rename once functionality has changed
+internal class SourceFileWriter {
 
     /**
      * Writes a collection of types to a source file OutputStream.
@@ -21,10 +23,9 @@ internal class SourceFileWriter { // TODO rename once functionality has changed
             sourceFile.addType(stack.pop())
         }
 
-        with(StringBuilder()) { // FIXME inefficient use of resources
-            sourceFile.build().writeTo(this)
-            output.write(toString().toByteArray())
-        }
+        val bufferedWriter = BufferedWriter(OutputStreamWriter(output))
+        sourceFile.build().writeTo(bufferedWriter)
+        bufferedWriter.flush()
     }
 
 }
