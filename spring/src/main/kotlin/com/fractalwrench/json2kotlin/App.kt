69e4956087f2f138a11e23f790aee6531ccbf38c
fractalwrench <fractalwrench@gmail.com>
2018-03-07 20:11:54 +0000

    update server port to 5000

--- spring/src/main/kotlin/com/fractalwrench/json2kotlin/App.kt
+++ spring/src/main/kotlin/com/fractalwrench/json2kotlin/App.kt
@@ -2,9 +2,15 @@ package com.fractalwrench.json2kotlin
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class Application
+class Application: SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(Application::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
